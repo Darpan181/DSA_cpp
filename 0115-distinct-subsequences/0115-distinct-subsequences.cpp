5a22bb0ef29d3d48f409bec6ed0822815c270c4f
Darpan181 <darpanpawar3820@gmail.com>
2026-09-06 09:22:22 +0530

    Time: 37 ms (25.09%), Space: 26.9 MB (72.62%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    long long find(int i, int j, string &a, string &b, vector<vector<long long>> &dp){
+    int find(int i, int j, string &s, string &t, vector<vector<int>> &dp){
         if(j < 0) return 1;
         if(i < 0) return 0;
+
         if(dp[i][j] != -1) return dp[i][j];
-        if(a[i] == b[j]){
-            return dp[i][j] = find(i - 1, j - 1, a, b, dp) + find(i - 1, j, a, b, dp);
-        }
-        else return dp[i][j] = find(i - 1, j, a, b, dp);
+
+        if(s[i] == t[j]) return dp[i][j] = find(i-1, j-1, s, t, dp) + find(i-1, j, s, t, dp);
+        else return dp[i][j] = find(i-1, j, s, t, dp);
     }
     int numDistinct(string s, string t) {
-        int n = s.length() , m = t.length();
-        vector<vector<long long>> dp(n , vector<long long> (m , -1));
-        return find(n - 1, m - 1, s, t, dp);
+        int m = s.length();
+        int n = t.length();
+        vector<vector<int>> dp(m , vector<int> (n , -1));
+        return find(m-1, n-1, s, t, dp);
     }
 };
